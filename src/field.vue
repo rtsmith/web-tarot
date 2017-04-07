d45d9525382e91608ea8fe86329b8575f67a72ce
Tom Smith <tom@solrr.net>
2017-04-07 12:12:45 -0600

    Make dynamic layout

--- src/field.vue
+++ src/field.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class=field :style="style">
-    <div v-for="cell in layout">
-      <div if="cell.order">
+  <div class=field :style="containerStyle">
+    <div v-for="cell in layout" :style="cellStyle(cell)">
         <p>{{ cell.order }}</p>
         <div v-if="drawnCards[cell.order - 1]">
           <img :src="imgPath(drawnCards[cell.order - 1].path)" />
         </div>
-      </div>
     </div>
   </div>
 </template>
@@ -14,16 +12,29 @@
 export default {
   data () {
     return {
-      style: {
-        // make these dynamic
-        "grid-template-columns": "1fr 1fr 1fr",
-        "grid-template-rows": "1fr 1fr 1fr"
+      cellStyle (cell) {
+        let style = {
+          "grid-row-start": cell.x + 1,
+          "grid-column-start": cell.y + 1
+        }
+        if (cell.cross)
+          style["transform"] = "rotate(90deg)"
+
+        return style
       }
     }
   },
   computed: {
     drawnCards () {
       return this.$store.state.drawnCards 
+    },
+    containerStyle () {
+      let x = Math.max(...this.$store.state.layout.map(v => v.x)) + 1
+      let y = Math.max(...this.$store.state.layout.map(v => v.y)) + 1
+      return {
+        "grid-template-rows": `repeat(${ x } 1fr)`,
+        "grid-template-columns": `repeat(${ y } 1fr)`
+      }
     },
     layout () {
       return this.$store.state.layout
@@ -44,13 +55,12 @@ export default {
   display: grid;
   width: 800px;
   height: 500px;
-  color: white;
+  color: black;
   img {
     max-width: 200px;
   }
 }
 .field > div {
-  background: purple;
   padding: 4px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -4,22 +4,40 @@ import cards from '../cards.json'
 
 Vue.use(Vuex)
 
-let layout = [
-  {x: 0, y: 0, order: null},
-  {x: 1, y: 0, order: 2},
-  {x: 2, y: 0, order: null},
-  {x: 0, y: 1, order: 5},
-  {x: 1, y: 1, order: 1},
-  {x: 2, y: 1, order: 4},
-  {x: 0, y: 2, order: null},
-  {x: 1, y: 2, order: 3},
-  {x: 2, y: 2, order: null},
-]
+let layouts = {
+  basic: [
+    {x: 2, y: 2, order: null},
+    {x: 0, y: 1, order: 5},
+    {x: 1, y: 1, order: 1},
+    {x: 0, y: 0, order: null},
+    {x: 1, y: 0, order: 2},
+    {x: 2, y: 1, order: 4},
+    {x: 0, y: 2, order: null},
+    {x: 2, y: 0, order: null},
+    {x: 1, y: 2, order: 3},
+  ],
+  celtic: [
+    {x: 0, y: 0, order: null},
+    {x: 1, y: 0, order: 3},
+    {x: 1, y: 2, order: 4},
+    {x: 2, y: 2, order: null},
+    {x: 0, y: 3, order: 10},
+    {x: 1, y: 3, order: 9},
+    {x: 2, y: 0, order: null},
+    {x: 0, y: 1, order: 6},
+    {x: 1, y: 1, order: 1},
+    {x: 1, y: 1, order: 2, cross: true},
+    {x: 2, y: 1, order: 5,},
+    {x: 0, y: 2, order: null},
+    {x: 2, y: 3, order: 8},
+    {x: 3, y: 3, order: 7},
+  ]
+}
 
 let store = new Vuex.Store({
   state: {
     deck: Object.create(cards),
-    layout: layout,
+    layout: layouts.celtic,
     drawnCards: []
   },
   mutations: {
